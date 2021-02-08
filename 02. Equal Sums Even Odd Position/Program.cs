c1d0a7e4ba715436e28959d201797412717d6189
Момчил Миланов <deadman_kula@abv.bg>
2021-02-08 18:48:06 +0200

    solved

--- 02. Equal Sums Even Odd Position/Program.cs
+++ 02. Equal Sums Even Odd Position/Program.cs
@@ -6,8 +6,34 @@ namespace _02._Equal_Sums_Even_Odd_Position
     {
         static void Main(string[] args)
         {
-            
-
+            int firstNum = int.Parse(Console.ReadLine());
+            int secondNum = int.Parse(Console.ReadLine());
+            int evenSum = 0, oddSum = 0;
+            for (int i = firstNum; i <= secondNum; i++)
+            {
+                string currNum = i.ToString();
+                for (int j = 0; j < currNum.Length; j++)
+                {
+                    int currDigit = int.Parse(currNum[j].ToString());
+                    if (j % 2 == 0)
+                    {
+                        evenSum += currDigit;
+                    }
+                    else
+                    {
+                        oddSum += currDigit;
+                    }
+                }
+                if (evenSum == oddSum)
+                {
+                    Console.Write(i + " ");
+                }
+                else
+                {
+                    evenSum = 0;
+                    oddSum = 0;
+                }
+            }
         }
     }
 }
