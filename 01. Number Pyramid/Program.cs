825bbaaf05566ed07c19a6f9b20f9d7fd6c4b5e4
Момчил Миланов <deadman_kula@abv.bg>
2021-02-08 16:49:02 +0200

    Add project files.

---
+++ 01. Number Pyramid/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace _01._Number_Pyramid
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int curr = 1;
+            bool isBigger = false;
+            for (int i = 1; i <= n; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    if (curr > n)
+                    {
+                        isBigger = true;
+                        break;
+                    }
+                    Console.Write(curr + " ");
+                    curr++;
+                }
+                if (isBigger)
+                {
+                    break;
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
