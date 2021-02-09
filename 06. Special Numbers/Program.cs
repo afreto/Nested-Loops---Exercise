d5f1fb2662dd3985fd4e4c563f13d34e5eccc2b6
Момчил Миланов <deadman_kula@abv.bg>
2021-02-09 17:43:47 +0200

    solved

---
+++ 06. Special Numbers/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _06._Special_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 1111; i < 9999; i++)
+            {
+                string convChar = i.ToString();
+                int currDigit1 = int.Parse(convChar[0].ToString());
+                int currDigit2 = int.Parse(convChar[1].ToString());
+                int currDigit3 = int.Parse(convChar[2].ToString());
+                int currDigit4 = int.Parse(convChar[3].ToString());
+                if (currDigit1 != 0 && currDigit2 != 0 && currDigit3 != 0 && currDigit4 != 0)
+                {
+                    if (n % currDigit1 == 0 && n % currDigit2 == 0 && n % currDigit3 == 0 && n % currDigit4 == 0)
+                    {
+                        Console.Write(convChar + " ");
+                    }
+                }
+            }
+
+        }
+    }
+}
